e35bba7cbb4cb27da04b6336b3a876eb2878fb64
murtazalw <murtaza_lw@yahoo.com>
2016-06-27 11:55:30 +0300

    add new line on line-7 "Second commit"

--- Test Git/src/example/GitMain.java
+++ Test Git/src/example/GitMain.java
@@ -4,5 +4,6 @@ public class GitMain {
 	public static void main(String args[]) {
 		System.out.println("hello world");
 		System.out.println("First commit");
+		System.out.println("Second commit");
 	}
 }
