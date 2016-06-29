a6cda7de04c296c5db769c287c60153672d74cb9
murtazalw <murtaza_lw@yahoo.com>
2016-06-29 10:47:39 +0300

    hello Test2 retry test 5

--- Test Git/src/example/Test2.java
+++ Test Git/src/example/Test2.java
@@ -7,4 +7,5 @@ System.out.println("hello Test2 retry test");
 System.out.println("hello Test2 retry test 2");
 System.out.println("hello Test2 retry test 3");
 System.out.println("hello Test2 retry test 4");
+System.out.println("hello Test2 retry test 5");
 }
